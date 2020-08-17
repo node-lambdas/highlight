50e0356f78fba9404bb687206da900143b395920
Darlan Alves <me@darlanalv.es>
2020-08-17 16:49:33 +0200

    feat: implement function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import { lambda } from '@node-lambdas/core';
+import { lambda, Format } from '@node-lambdas/core';
+import hljs from 'highlight.js';
 
 const configutation = {
   version: 1,
+  input: Format.Text,
+  output: Format.Text,
 };
 
-lambda(configutation, (input, output) => input.pipe(output));
+lambda(configutation, (input, output) => {
+  const language = input.headers['x-language'];
+
+  if (language) {
+    try {
+      output.send(hljs.highlight(language, input.body).value);
+      return;
+    } catch {}
+  }
+
+  output.send(hljs.highlightAuto(input.body).value);
+});
